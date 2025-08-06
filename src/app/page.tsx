4328eb7c229466f894c08d379faa8cdd3980fd02
Carlos Oliveira <cvmacedo7@gmail.com>
2025-08-06 18:56:19 -0300

    chore: initial settings for project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Home</h1>
+      <Button>Bootcamp</Button>
+    </div>
+  );
+}
+
+export default Home;
