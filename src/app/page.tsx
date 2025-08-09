514161e4d2c65e336498a77cd8f5ac7321212878
CPI314 <cvmacedo7@gmail.com>
2025-08-09 08:24:33 -0300

    feat: adjustments in product list and item components for better ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,14 @@ const Home = async () => {
   return (
     <>
       <Header />
-      <div className="space-y-6 px-5">
-        <Image src="/banner-01.png" alt="Leve uma vida com estilo" width={0} height={0} sizes="100vw" className="h-auto w-full"/>
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image src="/banner-01.png" alt="Leve uma vida com estilo" width={0} height={0} sizes="100vw" className="h-auto w-full"/>
+        </div>
         <ProductList product={products} title="Mais vendidos"/>
-        <Image src="/banner-02.png" alt="Seja autentico" width={0} height={0} sizes="100vw" className="h-auto w-full"/>
+        <div className="px-5">
+          <Image src="/banner-02.png" alt="Seja autentico" width={0} height={0} sizes="100vw" className="h-auto w-full"/>
+        </div>
       </div>
     </>
   );
--- src/components/common/product-list.tsx
+++ src/components/common/product-list.tsx
@@ -13,8 +13,8 @@ interface ProductListProps{
 const ProductList = async ({title, product}: ProductListProps) => {
   return(
     <div className="space-y-6">
-      <h3 className="font-semibold">{title}</h3>
-      <div className="flex w-full gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+      <h3 className="px-5 font-semibold">{title}</h3>
+      <div className="px-5 flex w-full gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
         {product.map(product => <ProductItem key={product.id} product={product}/>)}
       </div>
     </div>
