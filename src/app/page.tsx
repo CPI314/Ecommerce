11d681b2d836abaace1af092a4e79ea50016369f
CPI314 <cvmacedo7@gmail.com>
2025-08-09 10:08:29 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import {desc} from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
@@ -12,20 +15,45 @@ const Home = async () => {
     }
   })
   const categories = await db.query.categoryTable.findMany({});
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with:{
+      variants: true,
+    }
+  });
   return (
     <>
       <Header />
       <div className="space-y-6">
         <div className="px-5">
-          <Image src="/banner-01.png" alt="Leve uma vida com estilo" width={0} height={0} sizes="100vw" className="h-auto w-full"/>
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
         </div>
-        <ProductList product={products} title="Mais vendidos"/>
+
+        <ProductList product={products} title="Mais vendidos" />
+
         <div className="px-5">
-          <div className="px-5">
-            <CategorySelector categories={categories}/>
-          </div>
-          <Image src="/banner-02.png" alt="Seja autentico" width={0} height={0} sizes="100vw" className="h-auto w-full"/>
+          <CategorySelector categories={categories} />
         </div>
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+        <ProductList product={newlyCreatedProducts} title="Novos produtos" />
+        <Footer />
       </div>
     </>
   );
